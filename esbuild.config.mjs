aa6c725781b77235897efe7bff5eba61078d35fa
Newdea <9208450+Newdea@users.noreply.github.com>
2023-08-05 23:28:13 +0800

    fix buid err

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -7,7 +7,7 @@ const prod = process.argv[2] === "production";
 const context = await esbuild.context({
     entryPoints: ["src/main.ts"],
     bundle: true,
-    external: ["obsidian", "electron", ...builtins],
+    external: ["obsidian", "electron", "chart.js/dist/helpers/helpers.core", ...builtins],
     format: "cjs",
     target: "es2018",
     logLevel: "info",
